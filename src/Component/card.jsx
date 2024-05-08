d71db2beb7f8d413eb001a691dfa375d8184315c
ifBeans <162799827+ifBeans@users.noreply.github.com>
2024-05-09 00:52:57 +0700

    Card + Responsive

--- src/Component/card.jsx
+++ src/Component/card.jsx
@@ -9,6 +9,8 @@ import sibolangit from '../img/Hillpark-Sibolangit.png';
 import sorake from '../img/pantai-sorake.png';
 import maitreya from '../img/Vihara_Maitreya.png';
 import mashun from '../img/Masjid_Al_Mashun.png';
+import velangkani from '../img/Gereja_Velangkani_Medan.png';
+import funland from '../img/mikie-funland.png';
 
 
 const places = [
@@ -21,21 +23,23 @@ const places = [
     { name: 'Hillpark Sibolangit', image: sibolangit, shortDesc: 'King of The Hill' },
     { name: 'Pantai Sorake', image: sorake, shortDesc: 'Serokan' },
     { name: 'Vihara Maitreya', image: maitreya, shortDesc: 'Virya' },
-    { name: 'Masjid Al Mashun', image: mashun, shortDesc: 'Bismillah' }
+    { name: 'Masjid Al Mashun', image: mashun, shortDesc: 'Bismillah' },
+    { name: 'Gereja Velangkani Medan', image: velangkani, shortDesc: 'Mr Medan' },
+    { name: 'Mikie Funland', image: funland, shortDesc: 'Mickey Mouse' }
 ];
 
 const Card = () => {
     return (
-        <div className="flex flex-wrap justify-between mt-20 mx-60">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center mt-20 mx-10 md:mx-20 lg:mx-40">
             {places.map((place, index) => (
 
-                <div key={index} className="relative w-60 h-80 rounded shadow-lg m-2 overflow-hidden group bg-black">
-                    
-                    <img className="w-full h-full object-cover rounded transition duration-300 group-hover:opacity-50 scale-100 group-hover:scale-105 ease-in-out" src={place.image} alt={place.name} />
+                <div key={index} className="relative w-50 h-50 md:h-60 lg:h-70 rounded shadow-lg m-2 overflow-hidden group bg-black">
+
+                    <img className="w-full h-full object-cover rounded transition duration-300 group-hover:opacity-50 scale-100 group-hover:scale-110 ease-in-out" src={place.image} alt={place.name} />
 
                     <p className="opacity-0 group-hover:opacity-100 duration-300 absolute inset-0 flex justify-center items-start text-white font-semibold pt-5">{place.shortDesc}</p>
 
-                    <div className="font-bold text-sm text-white ms-2 mb-1 absolute bottom-0 left-0 bg-black opacity-80 rounded-full px-3">{place.name}</div>
+                    <div className="font-bold text-xs text-white ms-2 mb-1 absolute bottom-0 left-0 bg-black opacity-80 rounded-full px-3">{place.name}</div>
 
                 </div>
             ))}
