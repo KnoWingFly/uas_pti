d7d9c311e24a26078578472528a56fc2dd77ffdd
stevjoo <goodjestcom@gmail.com>
2024-05-08 10:55:48 +0700

    footer

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,8 +3,8 @@ import Logo from "../img/logo.png";
 
 export default function Footer() {
     return (
-        <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <footer class="bg-white shadow dark:bg-gray-900 w-full my-4">
+            <div class="w-full max-w-screen mx-auto p-4 md:py-8">
                 <div class="sm:flex sm:items-center sm:justify-between">
                     <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                         <img src={Logo} class="h-12" alt="Flowbite Logo" />
@@ -25,8 +25,10 @@ export default function Footer() {
                         </li>
                     </ul>
                 </div>
-                <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">Sumatera Utara™</a>. All Rights Reserved.</span>
+                    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <div className="flex">
+                    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">Sumatera Utara™</a>. All Rights Reserved.</span>
+                </div>
             </div>
         </footer>
 
