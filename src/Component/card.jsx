83766d488a27a7e5120c8dc3ccb42862ddb86af7
ifBeans <162799827+ifBeans@users.noreply.github.com>
2024-05-08 13:14:26 +0700

    Card

--- src/Component/card.jsx
+++ src/Component/card.jsx
@@ -1,33 +1,29 @@
 import React from 'react';
 import TobaLake from '../img/lake_toba.jpg';
 import muaratapanuli from '../img/Muara_Tapanuli.jpg';
-import lumbinipark from '../img/taman-alam-lumbini.jpg';
-import samosir from '../img/samosir.jpg';
+import nias from '../img/nias.jpg';
+import Tanjung_Buntu from '../img/Pantai_Tanjung_Buntu.jpeg';
+import Laut_Tawar from '../img/Laut_Tawar.jpg';
 
 const places = [
-    { name: 'Danau Toba', image: TobaLake },
-    { name: 'Muara Tapanuli Utara', image: muaratapanuli },
-    { name: 'Taman Alam Lumbini', image: lumbinipark },
-    { name: 'Pulau Samosir', image: samosir }
+    { name: 'Danau Toba', image: TobaLake},
+    { name: 'Muara Tapanuli Utara', image: muaratapanuli},
+    { name: 'Pulau Nias', image: nias},
+    { name: 'Pantai Tanjung Buntu', image: Tanjung_Buntu},
+    { name: 'Laut Tawar', image: Laut_Tawar}
 ];
 
 const Card = () => {
     return (
-        <div className="flex flex-wrap justify-between mt-20">
+        <div className="flex flex-wrap justify-between mt-20 mx-60">
             {places.map((place, index) => (
-                <div key={index} className="bg-white max-w-sm rounded overflow-hidden shadow-lg m-2 flex-auto">
-                    <img className="w-full" src={place.image} alt={place.name} />
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-black">{place.name}</div>
-                        <p className="text-gray-700 text-base">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                        </p>
-                    </div>
-                    <div className="px-6 pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                    </div>
+
+                <div key={index} className="relative w-60 h-80 rounded shadow-lg m-2 overflow-hidden">
+
+                    <img className="w-full h-full object-cover rounded hover:scale-125 ease-in duration-150" src={place.image} alt={place.name}/>
+
+                    <div className="font-bold text-sm text-white ms-2 mb-1 absolute bottom-0 left-0 bg-black opacity-80 rounded-full px-3">{place.name}</div>
+
                 </div>
             ))}
         </div>
