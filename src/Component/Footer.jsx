b879d5551edc1e861c4db6c0082e1fd539a4e834
stevjoo <goodjestcom@gmail.com>
2024-05-08 12:16:24 +0700

    perbaiki displaynya dikit

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -7,10 +7,10 @@ export default function Footer() {
             <div class="w-full max-w-screen mx-auto p-4 md:py-8">
                 <div class="sm:flex sm:items-center sm:justify-between">
                     <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src={Logo} class="h-12" alt="Flowbite Logo" />
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sumatera Utara</span>
+                        <img src={Logo} class="h-36" alt="Sumatera Utara Logo" />
+                        <span class="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">Sumatera Utara</span>
                     </a>
-                    <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <ul class="flex flex-wrap items-center mb-6 text-lg font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
                             <a href="#" class="hover:underline me-4 md:me-6">About</a>
                         </li>
@@ -25,10 +25,8 @@ export default function Footer() {
                         </li>
                     </ul>
                 </div>
-                    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <div className="flex">
-                    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">Sumatera Utara™</a>. All Rights Reserved.</span>
-                </div>
+                <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">Sumatera Utara™</a>. All Rights Reserved.</span>
             </div>
         </footer>
 
