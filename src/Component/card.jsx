36c5b93769573360f64da5b54323e2e43b4d274c
ifBeans <162799827+ifBeans@users.noreply.github.com>
2024-05-08 16:04:21 +0700

    Card

--- src/Component/card.jsx
+++ src/Component/card.jsx
@@ -12,16 +12,16 @@ import mashun from '../img/Masjid_Al_Mashun.png';
 
 
 const places = [
-    { name: 'Danau Toba', image: TobaLake},
-    { name: 'Muara Tapanuli Utara', image: muaratapanuli},
-    { name: 'Pulau Samosir', image: samosir},
-    { name: 'Bukit Holbung', image: holbung},
-    { name: 'Gedung London Sumatera', image: gerejaLondon},
-    { name: 'Istana Maimun', image: maimun},
-    { name: 'Hillpark Sibolangit', image: sibolangit},
-    { name: 'Pantai Sorake', image: sorake},
-    { name: 'Vihara Maitreya', image: maitreya},
-    { name: 'Masjid Al Mashun', image: mashun}
+    { name: 'Danau Toba', image: TobaLake, shortDesc: 'mmm Lake'},
+    { name: 'Muara Tapanuli Utara', image: muaratapanuli , shortDesc: 'Nordt'},
+    { name: 'Pulau Samosir', image: samosir, shortDesc: 'Samosa' },
+    { name: 'Bukit Holbung', image: holbung, shortDesc: 'Hobit' },
+    { name: 'Gedung London Sumatera', image: gerejaLondon, shortDesc: 'Dondon' },
+    { name: 'Istana Maimun', image: maimun, shortDesc: 'Timun' },
+    { name: 'Hillpark Sibolangit', image: sibolangit, shortDesc: 'King of The Hill' },
+    { name: 'Pantai Sorake', image: sorake, shortDesc: 'Serokan' },
+    { name: 'Vihara Maitreya', image: maitreya, shortDesc: 'Virya' },
+    { name: 'Masjid Al Mashun', image: mashun, shortDesc: 'Bismillah' }
 ];
 
 const Card = () => {
@@ -29,9 +29,11 @@ const Card = () => {
         <div className="flex flex-wrap justify-between mt-20 mx-60">
             {places.map((place, index) => (
 
-                <div key={index} className="relative w-60 h-80 rounded shadow-lg m-2 overflow-hidden">
+                <div key={index} className="relative w-60 h-80 rounded shadow-lg m-2 overflow-hidden group">
+                    
+                    <img className="w-full h-full object-cover rounded transition duration-300 group-hover:opacity-50 scale-100 group-hover:scale-110 ease-in-out" src={place.image} alt={place.name} />
 
-                    <img className="w-full h-full object-cover rounded hover:scale-125 ease-in duration-150" src={place.image} alt={place.name}/>
+                    <p className="opacity-0 group-hover:opacity-100 duration-300 absolute inset-0 flex justify-center items-start text-white font-semibold pt-5">{place.shortDesc}</p>
 
                     <div className="font-bold text-sm text-white ms-2 mb-1 absolute bottom-0 left-0 bg-black opacity-80 rounded-full px-3">{place.name}</div>
 
