b872ef1d5bf0b23d02c12947aca7298f2827f36c
KnowingFly <UnKnownOwl26@gmail.com>
2024-05-09 16:33:21 +0700

    Pisahin data ama bikin isi dari card (blm jadi total)

---
+++ src/Component/DataPlace.js
@@ -0,0 +1,14 @@
+export const places = [
+    { name: 'Danau Toba', image: require('../img/lake_toba.jpg'), shortDesc: 'mmm Lake'},
+    { name: 'Muara Tapanuli Utara', image: require('../img/Muara_Tapanuli.jpg'), shortDesc: 'Nordt'},
+    { name: 'Pulau Samosir', image: require('../img/samosir.jpg'), shortDesc: 'Samosa' },
+    { name: 'Bukit Holbung', image: require('../img/bukit-holbung.png'), shortDesc: 'Hobit' },
+    { name: 'Gedung London Sumatera', image: require('../img/Gedung-London-Sumatera.png'), shortDesc: 'Dondon' },
+    { name: 'Istana Maimun', image: require('../img/Istana_maimun.png'), shortDesc: 'Timun' },
+    { name: 'Hillpark Sibolangit', image: require('../img/Hillpark-Sibolangit.png'), shortDesc: 'King of The Hill' },
+    { name: 'Pantai Sorake', image: require('../img/pantai-sorake.png'), shortDesc: 'Serokan' },
+    { name: 'Vihara Maitreya', image: require('../img/Vihara_Maitreya.png'), shortDesc: 'Virya' },
+    { name: 'Masjid Al Mashun', image: require('../img/Masjid_Al_Mashun.png'), shortDesc: 'Bismillah' },
+    { name: 'Gereja Velangkani Medan', image: require('../img/Gereja_Velangkani_Medan.png'), shortDesc: 'Mr Medan' },
+    { name: 'Mikie Funland', image: require('../img/mikie-funland.png'), shortDesc: 'Mickey Mouse' }
+];
--- src/Component/card.jsx
+++ src/Component/card.jsx
@@ -1,50 +1,100 @@
-import React from 'react';
-import TobaLake from '../img/lake_toba.jpg';
-import muaratapanuli from '../img/Muara_Tapanuli.jpg';
-import samosir from '../img/samosir.jpg';
-import holbung from '../img/bukit-holbung.png';
-import gerejaLondon from '../img/Gedung-London-Sumatera.png';
-import maimun from '../img/Istana_maimun.png';
-import sibolangit from '../img/Hillpark-Sibolangit.png';
-import sorake from '../img/pantai-sorake.png';
-import maitreya from '../img/Vihara_Maitreya.png';
-import mashun from '../img/Masjid_Al_Mashun.png';
-import velangkani from '../img/Gereja_Velangkani_Medan.png';
-import funland from '../img/mikie-funland.png';
-
-
-const places = [
-    { name: 'Danau Toba', image: TobaLake, shortDesc: 'mmm Lake'},
-    { name: 'Muara Tapanuli Utara', image: muaratapanuli , shortDesc: 'Nordt'},
-    { name: 'Pulau Samosir', image: samosir, shortDesc: 'Samosa' },
-    { name: 'Bukit Holbung', image: holbung, shortDesc: 'Hobit' },
-    { name: 'Gedung London Sumatera', image: gerejaLondon, shortDesc: 'Dondon' },
-    { name: 'Istana Maimun', image: maimun, shortDesc: 'Timun' },
-    { name: 'Hillpark Sibolangit', image: sibolangit, shortDesc: 'King of The Hill' },
-    { name: 'Pantai Sorake', image: sorake, shortDesc: 'Serokan' },
-    { name: 'Vihara Maitreya', image: maitreya, shortDesc: 'Virya' },
-    { name: 'Masjid Al Mashun', image: mashun, shortDesc: 'Bismillah' },
-    { name: 'Gereja Velangkani Medan', image: velangkani, shortDesc: 'Mr Medan' },
-    { name: 'Mikie Funland', image: funland, shortDesc: 'Mickey Mouse' }
-];
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import {places} from './DataPlace.js';
 
 const Card = () => {
-    return (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center mt-20 mx-10 md:mx-20 lg:mx-40">
-            {places.map((place, index) => (
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(null);
 
-                <div key={index} className="relative w-50 h-50 md:h-60 lg:h-70 rounded shadow-lg m-2 overflow-hidden group bg-black">
+  const handleClick = (place, index) => {
+    setIsOpen(true);
+    setSelectedPlace(place);
+    setCurrentIndex(index);
+  };
 
-                    <img className="w-full h-full object-cover rounded transition duration-300 group-hover:opacity-50 scale-100 group-hover:scale-110 ease-in-out" src={place.image} alt={place.name} />
+  const handleNext = () => {
+    const nextIndex = (currentIndex + 1) % places.length;
+    setSelectedPlace(places[nextIndex]);
+    setCurrentIndex(nextIndex);
+  };
 
-                    <p className="opacity-0 group-hover:opacity-100 duration-300 absolute inset-0 flex justify-center items-start text-white font-semibold pt-5">{place.shortDesc}</p>
-
-                    <div className="font-bold text-xs text-white ms-2 mb-1 absolute bottom-0 left-0 bg-black opacity-80 rounded-full px-3">{place.name}</div>
-
-                </div>
-            ))}
+return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center mt-20 mx-10 md:mx-20 lg:mx-40">
+      {places.map((place, index) => (
+        <div
+          key={index}
+          className="relative w-50 h-50 md:h-60 lg:h-70 rounded shadow-lg m-2 overflow-hidden group bg-black"
+          onClick={() => handleClick(place, index)}
+        >
+          <img className="w-full h-full object-cover rounded transition duration-300 group-hover:opacity-50 scale-100 group-hover:scale-110 ease-in-out" src={place.image} alt={place.name} />
+          <p className="opacity-0 group-hover:opacity-100 duration-300 absolute inset-0 flex justify-center items-start text-white font-semibold pt-5">{place.shortDesc}</p>
+          <div className="font-bold text-xs text-white ms-2 mb-1 absolute bottom-0 left-0 bg-black opacity-80 rounded-full px-3">{place.name}</div>
         </div>
-    );
-};
+      ))}
+      <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="fixed z-10 inset-0 overflow-y-auto"
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              className="fixed inset-0 transition-opacity"
+            >
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </motion.div>
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+            <motion.div
+              initial={{ y: "100vh" }}
+              animate={{ y: 0 }}
+              exit={{ y: "100vh" }}
+              className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-full sm:w-full h-full"
+              style={{ width: '1100px', height: '500px' }}
+              >
+              <button
+                  onClick={() => setIsOpen(false)}
+                  type="button"
+                  className="absolute top-0 right-0 m-2 inline-flex justify-center rounded-md border border-transparent px-2 py-1 bg-indigo-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+              >
+                  X
+              </button>
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 flex h-full">
+                  <div className="w-1/2 h-full overflow-auto">
+                  <img src={selectedPlace.image} alt={selectedPlace.name} className="w-full object-contain h-full" />
+                  </div>
+                  <div className="w-1/2 p-4 overflow-auto">
+                  <h3 className="text-lg leading-6 font-medium text-gray-900">
+                      {selectedPlace.name}
+                  </h3>
+                  <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                      {selectedPlace.shortDesc}
+                      </p>
+                  </div>
+                  </div>
+              <div className="absolute bottom-0 right-0 m-4">
+                  <button
+                      onClick={handleNext}
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent px-4 py-2 bg-indigo-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                  >
+                      Next
+                  </button>
+              </div>
+              </div>
+            </motion.div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  </div>
+        );
+}
 
 export default Card;
