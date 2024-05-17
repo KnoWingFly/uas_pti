67f722edc653a8fe9d94c9d744e4ea79e9f5eed5
ifBeans <fabiandustin27@gmail.com>
2024-05-17 09:03:55 +0700

    footer responsive

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
                         <span className="self-center text-2xl md:text-4xl font-semibold whitespace-nowrap text-white hover:text-green-600">Sumatera Utara</span>
                     </a>
 
-                    <ul className="flex flex-wrap mt-2 items-center mb-6 text-lg font-medium text-white md:mb-0">
+                    <ul className="flex flex-wrap mt-2 items-center mb-6 text-lg md:ms-16 font-medium text-white md:mb-0">
                         <li>
                             <a href="/" className="hover:underline me-4 md:me-6 hover:text-green-600">Main</a>
                         </li>
