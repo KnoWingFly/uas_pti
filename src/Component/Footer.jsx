b9d6c987d6b11ae83e5d438c58d4679d1081223e
stevjoo <goodjestcom@gmail.com>
2024-05-18 15:05:08 +0700

    akhirnya bisa redirect di hostinger

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from "../img/logo.png";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-900 mt-4">
+        <footer className="bg-black mt-4">
             <div className="w-full max-w-screen p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
@@ -15,13 +15,13 @@ export default function Footer() {
                             <a href="/" className="hover:underline me-4 md:me-6 hover:text-green-600">Home</a>
                         </li>
                         <li>
-                            <a href="Category" className="hover:text-green-600 hover:underline me-4 md:me-6">Category</a>
+                            <a href="/category/" className="hover:text-green-600 hover:underline me-4 md:me-6">Category</a>
                         </li>
                         <li>
-                            <a href="Aboutus" className="hover:text-green-600 hover:underline me-4 md:me-6">About Us</a>
+                            <a href="/aboutus/" className="hover:text-green-600 hover:underline me-4 md:me-6">About Us</a>
                         </li>
                         <li>
-                            <a href="https://github.com/KnoWingFly/uas_pti" className="hover:text-green-600 hover:underline">GitHub</a>
+                            <a target="_blank" href="https://github.com/KnoWingFly/uas_pti" className="hover:text-green-600 hover:underline">GitHub</a>
                         </li>
                     </ul>
                 </div>
